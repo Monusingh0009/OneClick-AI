8e3fb86a135d4b2b868758e9a33a6252d649f2a7
Monusingh0009 <monusinghryder007@gmail.com>
2025-10-09 02:12:54 +0530

    first commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="md:max-w-96">
           <img className="h-9" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
-            Experience the power of AI with QuickAi. <br /> Transform you
+            Experience the power of AI with OneClick.ai. <br /> Transform you
             content creation with our suite of premium AI tools. Write articles,
             generate images, and enhance your workflow.
           </p>
@@ -54,11 +54,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="pt-4 text-center text-xs md:text-sm pb-5">
-        Copyright 2025 ©{" "}
-        <a target="_blank" href="https://elyse502.github.io/Elysee-Portfolio">
-          ElyséeDev
-        </a>
-        . All Right Reserved.
+        Copyright 2025 @ OneClick.ai. All Right Reserved.
       </p>
     </footer>
   );
